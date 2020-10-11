320eb96e57f3c0196e5aad99acddff00a89d96d7
jeffreygatbonton <jeff@creationcrate.com>
2020-10-11 15:10:44 -0400

    set up env variables, access token is returning

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
-const posts =[
+app.use(express.json());
+
+const posts = [
   {
     username: 'El Jefe',
-    title: 'post 1'
+    title: 'post 1',
   },
   {
     username: 'Jane Doe',
-    title: 'post 2'
+    title: 'post 2',
   },
-]
+];
 
 app.get('/posts', (req, res) => {
-  res.json(posts)
-})
+  res.json(posts);
+});
 
-app.get('./login', (req, res) => {
-  
-})
+app.post('/login', (req, res) => {
+  // auth user
 
-app.listen(3000)
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken });
+});
+
+app.listen(3000);

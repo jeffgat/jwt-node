df63204dc1fa95a66793a25eb076deaf827aa08b
jeffreygatbonton <jeff@creationcrate.com>
2020-10-11 15:50:57 -0400

    split servers into 2, added creation and deletion of refresh tokens

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(express.json());
 
@@ -16,18 +16,22 @@ const posts = [
   },
 ];
 
-app.get('/posts', (req, res) => {
-  res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
+  // only return posts that are the user's
 });
 
-app.post('/login', (req, res) => {
-  // auth user
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  // splitting at the space in "Bearer (TOKEN)"
+  if (token === null) return res.sendStatus(401);
 
-  const username = req.body.username;
-  const user = { name: username };
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken });
-});
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user
+    next()
+  });
+}
 
 app.listen(3000);
